f13f11d98e3b6f9eb8fd483090b16c017436bf3a
hzwy23 <hzwy23@163.com>
2017-03-27 09:15:14 +0800

    update

--- apps/init.go
+++ apps/init.go
@@ -1,5 +1,16 @@
 package apps
 
-func init(){
+import (
+	"github.com/hzwy23/asofdate/apps/mas/ftp"
+	"github.com/hzwy23/asofdate/apps/mas/ca"
+	"github.com/hzwy23/asofdate/apps/mas/common"
+)
 
+// 在init中注册自己应用的路由
+func init(){
+	// 系统开启路由服务之前,会先执行这个init中的函数
+	// 系统默认采用beego的路由
+	//ca.Register()
+	//ftp.Register()
+	//common.Register()
 }
