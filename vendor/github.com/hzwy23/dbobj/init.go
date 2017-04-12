30ec1e291c7295b92fd2ba803382360c7153e7fc
hzwy23 <hzwy23@163.com>
2017-04-13 00:40:09 +0800

    update

--- vendor/github.com/hzwy23/dbobj/init.go
+++ vendor/github.com/hzwy23/dbobj/init.go
@@ -2,20 +2,20 @@ package dbobj
 
 import (
 	_ "github.com/hzwy23/dbobj/mysql"
-	_ "github.com/hzwy23/dbobj/oracle"
+	//_ "github.com/hzwy23/dbobj/oracle"
+	"github.com/hzwy23/dbobj/utils"
 	"os"
 	"path/filepath"
-	"github.com/hzwy23/dbobj/utils"
 )
 
-func init(){
-	HOME:=os.Getenv("HBIGDATA_HOME")
-	filedir := filepath.Join(HOME,"conf/system.properties")
-	conf,err := utils.GetConfig(filedir)
+func init() {
+	HOME := os.Getenv("HBIGDATA_HOME")
+	filedir := filepath.Join(HOME, "conf/system.properties")
+	conf, err := utils.GetConfig(filedir)
 	if err != nil {
 		panic("init database failed.")
 	}
-	Default,err = conf.Get("DB.type")
+	Default, err = conf.Get("DB.type")
 	if err != nil {
 		panic("get default database type failed.")
 	}
